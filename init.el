55a87705ffb503063ad9871e66651385a477a411
Ochirsum <ochirsum74@gmail.com>
2020-09-04 12:37:19 +0800

    add config

--- init.el
+++ init.el
@@ -4,3 +4,14 @@
 (scroll-bar-mode -1)
 
 (setq make-backup-files nil)
+
+(global-set-key (kbd "C-x C-h") help-map)
+(global-set-key (kbd "C-h") 'backward-kill-word)
+(global-set-key (kbd "C-t") 'previous-line)
+
+(global-set-key [?\C-.] 'execute-extended-command)
+(global-set-key [?\C-,] (lookup-key global-map [?\C-x]))
+(global-set-key [?\C-'] 'hippie-expand)
+
+(keyboard-translate ?\C-x ?\C-u)
+(keyboard-translate ?\C-u ?\C-x)
